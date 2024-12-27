2b5f4fd54a6fc1b44171a6713481246c75b63aa5
HackerDoggo101 <126309004+HackerDoggo101@users.noreply.github.com>
2024-12-27 10:48:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cnbk0ovsgzcf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jason-employee"
+customregion = "us-east-1"
+
